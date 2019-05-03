8956967de32e0fe475afc5383d7333fa7903c586
CodyPolton <43789835+CodyPolton@users.noreply.github.com>
2019-05-03 10:41:04 -0500

    Update RJI24.sql

--- Code/RJI24.sql
+++ Code/RJI24.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Apr 22, 2019 at 08:17 PM
--- Server version: 10.1.35-MariaDB
--- PHP Version: 7.2.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -22,19 +13,6 @@ SET time_zone = "+00:00";
 -- Database: `SoftwareDev(RJI)`
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `Has`
---
-
-CREATE TABLE `Has` (
-  `Title` varchar(255) NOT NULL,
-  `Username` varchar(24) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `Photos`
 --
@@ -42,6 +20,7 @@ CREATE TABLE `Has` (
 CREATE TABLE `Photos` (
   `Picture` varchar(70) NOT NULL,
   `Title` varchar(255) NOT NULL,
+  `Username` varchar(24) NOT NULL,
   `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Score` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -63,35 +42,32 @@ CREATE TABLE `User` (
 --
 
 --
--- Indexes for table `Has`
---
-ALTER TABLE `Has`
-  ADD PRIMARY KEY (`Username`,`Title`),
-  ADD KEY `Title` (`Title`);
 
 --
 -- Indexes for table `Photos`
 --
 ALTER TABLE `Photos`
-  ADD PRIMARY KEY (`Title`);
+  ADD PRIMARY KEY (`Title`, `Username`);
+ 
 
 --
 -- Indexes for table `User`
 --
 ALTER TABLE `User`
   ADD PRIMARY KEY (`Username`);
+  
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `Has`
 --
-ALTER TABLE `Has`
-  ADD CONSTRAINT `Has_ibfk_1` FOREIGN KEY (`Title`) REFERENCES `Photos` (`Title`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `Has_ibfk_2` FOREIGN KEY (`Username`) REFERENCES `User` (`Username`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Photos`
+  
+  ADD CONSTRAINT `Photos_ibfk_2` FOREIGN KEY (`Username`) REFERENCES `User` (`Username`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
